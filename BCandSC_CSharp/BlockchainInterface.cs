b512ad15b373681d6c227d6a34704f6744f89983
Zandian, Parviz <parviz.zandian@student.fhws.de>
2023-02-04 16:44:03 +0100

    implemented api for the contract

--- BCandSC_CSharp/BlockchainInterface.cs
+++ BCandSC_CSharp/BlockchainInterface.cs
@@ -7,6 +7,8 @@ using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
 using Betting.Betting.ContractDefinition;
 using Betting.Betting;
+using Nethereum.RPC.Eth.DTOs;
+using Newtonsoft.Json.Linq;
 
 
 namespace BCandSC_CSharp
@@ -14,12 +16,14 @@ namespace BCandSC_CSharp
     public class BlockchainInterface
     {
         private const string Url = "HTTP://192.168.178.27:7545";
+        // this is the private key of the contract owner
         private const string PrivateKey = "0x840af416508488a7ef3f7e822d71b381235bae6c1286f9203d1d89818a7dcb47";
         private const string ContractAddress = "0x9314658625A4a4c212859Abd15D6A168aE6d6731";
         private Account _account;
         private Web3 _web3;
         private BettingService _bettingService;
         private const long gas = 20000000000;
+        // TODO increase the betting value for easier testing
         private const long value = 100000000000000;
 
 
@@ -33,19 +37,60 @@ namespace BCandSC_CSharp
 
         public static void Main()
         {
-            Console.WriteLine("test");
+            var blockchainInterface = new BlockchainInterface();
         }
 
-
-        public void Bet(string teamRepresentation)
+        public JArray Bet(string teamRepresentation, string privateKey)
         {
-            _bettingService.BetRequestAndWaitForReceiptAsync(new BetFunction()
-                { TeamRepresentation = teamRepresentation, Gas = gas, AmountToSend = value });
+            _account = new Account(privateKey);
+            _web3 = new Web3(_account, Url);
+            _bettingService = new BettingService(_web3, ContractAddress);
+
+            return _bettingService.BetRequestAndWaitForReceiptAsync(new BetFunction()
+                    { TeamRepresentation = teamRepresentation, GasPrice = gas, AmountToSend = value })
+                .Result.Logs;
         }
+
+        public JArray DistributePrices(string teamRepresentationOfWinner, string privateKey)
+        {
+            _account = new Account(privateKey);
+            _web3 = new Web3(_account, Url);
+            _bettingService = new BettingService(_web3, ContractAddress);
+
+            return _bettingService.DistributePrizesRequestAndWaitForReceiptAsync(new DistributePrizesFunction()
+                    { TeamRepresentationOfWinner = teamRepresentationOfWinner, GasPrice = gas })
+                .Result.Logs;
+        }
+
+        public JArray StartGame(string privateKey)
+        {
+            _account = new Account(privateKey);
+            _web3 = new Web3(_account, Url);
+            _bettingService = new BettingService(_web3, ContractAddress);
+            
+            return _bettingService.StartGameRequestAndWaitForReceiptAsync(new StartGameFunction() { GasPrice = gas })
+                .Result.Logs;
+        }
+        
+        public JArray FinishGame(string privateKey)
+        {
+            _account = new Account(privateKey);
+            _web3 = new Web3(_account, Url);
+            _bettingService = new BettingService(_web3, ContractAddress);
+            
+            return _bettingService.FinishGameRequestAndWaitForReceiptAsync(new FinishGameFunction() { GasPrice = gas })
+                .Result.Logs;
+        }
+
 
         public BigInteger GetMoneyPool()
         {
             return _bettingService.GetMoneyPoolQueryAsync().Result;
+        }
+
+        public BigInteger GetPlayerCount()
+        {
+            return _bettingService.GetPlayerCountQueryAsync().Result;
         }
 
 
@@ -55,31 +100,37 @@ namespace BCandSC_CSharp
             {
                 // Setup
                 // Here we're using local chain eg Geth https://github.com/Nethereum/TestChains#geth
-                var account = new Account(PrivateKey);
-                var web3 = new Web3(account, Url);
+                //var account = new Account(PrivateKey);
+                //var web3 = new Web3(account, Url);
 
                 //Console.WriteLine("Deploying...");
                 //var deployment = new BettingDeployment();
                 // var receipt = await BettingService.DeployContractAndWaitForReceiptAsync(web3, deployment);
-                var service = new BettingService(web3, ContractAddress);
+                //var service = new BettingService(web3, ContractAddress);
                 //Console.WriteLine($"Contract Deployment Tx Status: {receipt.Status.Value}");
                 //Console.WriteLine($"Contract Address: {service.ContractHandler.ContractAddress}");
                 //Console.WriteLine("");
 
-                Console.WriteLine("Sending a transaction to the function set()...");
+                Console.WriteLine("Start.");
 
+                Console.WriteLine("First Bet...");
+                Console.WriteLine(Bet("HAHA", "0x1c7a3d64bf03314cd99cbadc1aac27a96512efb1c5adc2e3994f0533be1571da"));
+                Console.WriteLine("Second Bet...");
+                Console.WriteLine(Bet("BEBE", "0xf3b96df77469e17a768461fd086071ee06fdf662bad857bc56dfa1d50dd164fb"));
+                Console.WriteLine("Start Game...");
+                Console.WriteLine(StartGame(PrivateKey));
+                Console.WriteLine("Finish Game...");
+                Console.WriteLine(FinishGame(PrivateKey));
+                Console.WriteLine("MoneyPool:");
                 Console.WriteLine(GetMoneyPool());
-                // var receiptForSetFunctionCall = await service.SetRequestAndWaitForReceiptAsync(
-                //     new SetFunction() { X = 42, Gas = 400000 });
-                // Console.WriteLine($"Finished storing an int: Tx Hash: {receiptForSetFunctionCall.TransactionHash}");
-                // Console.WriteLine($"Finished storing an int: Tx Status: {receiptForSetFunctionCall.Status.Value}");
-                // Console.WriteLine("");
-                //
-                // Console.WriteLine("Calling the function get()...");
-                // var intValueFromGetFunctionCall = await service.GetQueryAsync();
-                // Console.WriteLine($"Int value: {intValueFromGetFunctionCall} (expecting value 42)");
-                // Console.WriteLine("");
-                
+                Console.WriteLine("Player count:");
+                Console.WriteLine(GetPlayerCount());
+                Console.WriteLine("Distribute Prices:");
+                Console.WriteLine(DistributePrices("BEBE", PrivateKey));
+                Console.WriteLine("MoneyPool:");
+                Console.WriteLine(GetMoneyPool());
+                Console.WriteLine("Player count:");
+                Console.WriteLine(GetPlayerCount());
             }
             catch (Exception ex)
             {
