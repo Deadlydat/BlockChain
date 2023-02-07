3358a36f76e6003f927bb4ee18f58c562cd45ca7
Emmel, Nicolas <nicolas.emmel@student.fhws.de>
2023-02-07 15:55:47 +0100

    Zwischen

--- BCandSC_CSharp/Pages/PlayerSelection.cshtml.cs
+++ BCandSC_CSharp/Pages/PlayerSelection.cshtml.cs
@@ -69,8 +69,9 @@ namespace BCandSC_CSharp.Pages
                 User user = new User();
                 user = user.GetUser(UserId);
 
-                //BlockchainInterface blockchainInterface = new BlockchainInterface();
-                //blockchainInterface.Bet(team.Name, user.PrivateKey);
+                BlockchainInterface blockchainInterface = new BlockchainInterface();
+                blockchainInterface.Bet(team.Name, user.PrivateKey);
+                team.SetTeamDone(true, team.Id);
 
            
                 return RedirectToPage("/Matchday", new { userId = UserId });
