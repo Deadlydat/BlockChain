b1c2c49da4871b2928c510fa00cfe37f15283097
Deadlydat <81553941+Deadlydat@users.noreply.github.com>
2023-02-07 16:07:21 +0100

    changed backend

--- BCandSC_CSharp/Pages/Simulation.cshtml.cs
+++ BCandSC_CSharp/Pages/Simulation.cshtml.cs
@@ -6,8 +6,8 @@ namespace BCandSC_CSharp.Pages
     public class SimulationModel : PageModel
     {
 
-        public Gamelogic gamelogic =new(Enviroment.GetEnviroment().Matchday);
-        public BlockchainInterface BlockchainInterface=new ();
+        public Gamelogic gamelogic = new(Enviroment.GetEnviroment().Matchday);
+        public BlockchainInterface BlockchainInterface = new();
         private String adminKey = "0xc815fa507181842ea2c85b12d75dc162b633d1c69d87e351bb86406f3db5b72f";
 
 
@@ -20,39 +20,38 @@ namespace BCandSC_CSharp.Pages
         public IActionResult OnPost()
         {
 
-            if (Request.Query["method"] == "startmatchday")
-            {
-                //gamelogic = new Gamelogic(Enviroment.GetEnviroment().Matchday);
-                //BlockchainInterface = new BlockchainInterface();
-                Console.WriteLine("started Matchday now you can bet");
-
-            }
 
             if (Request.Query["method"] == "startgame")
             {
-                //gamelogic.UsersId = gamelogic.GetUsersForMatchDay();
+                Console.WriteLine("starting game...");
                 BlockchainInterface.StartGame(adminKey);
                 Console.WriteLine("started game betting is closed");
 
             }
+
+
             if (Request.Query["method"] == "endgame")
             {
+                Console.WriteLine("game is almost over..");
                 BlockchainInterface.FinishGame(adminKey);
                 Console.WriteLine("finished game");
 
 
-            }
-
-            if (Request.Query["method"] == "endmatchday")
-            {
                 Team winnerTeam = gamelogic.GetResultsForMatch();
                 BlockchainInterface.DistributePrices(winnerTeam.Name, adminKey);
-                Console.WriteLine("winner team " + winnerTeam.Name + " end machtday");
-
+                Console.WriteLine("winner team: " + winnerTeam.Name + "-> end matchday");
                 //matchday +1
+
+            }
+
+
+
+            if (Request.Query["method"] == "money")
+            {
 
 
             }
+
 
 
             return Page();
